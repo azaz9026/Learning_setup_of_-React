de3a1f63f44d9be6ad9c94ff4e718bd29be98989
Md Azaz <mdazaz9026@gmail.com>
2023-09-11 20:14:33 +0530

    count increment and decremen by useState

--- VitwProject/src/App.jsx
+++ VitwProject/src/App.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
+
 
 function App() {
+
   const userName = "Chai aur Code"
+
+ // let counter = 13
+
+ let [counter , setCounter ] = useState(15)
+
+  const addValue = () => {
+     
+    //counter = counter + 1;
+    setCounter(counter + 1);
+   
+  }
+
+  const removeValue = ()=>{
+
+    setCounter(counter - 1);
+  
+  }
+
+
   return (
     <>
-      <h1> Chai aur Code with React  ||  Azaz {userName}</h1>
+      <h1>Chai Aur React</h1>
+      <h2>Counter Value : {counter} </h2>
+
+      <button onClick={addValue}
+      >Add Value </button>
+
+      <br/>
+      
+      <button onClick={removeValue}
+      >Remove Value </button>
     </>
   )
 }
